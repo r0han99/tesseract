079b22d7e5aea0f880c7c395cc9282b66ca307a7
r0han99 <r409067@gmail.com>
2023-04-14 17:02:48 -0600

    added content

--- website.py
+++ website.py
@@ -34,3 +34,5 @@ elif category == 'Support Vector Machines':
 
     fig = px.bar(df, x="continent", y="pop", color="continent",
     animation_frame="year", animation_group="country", range_y=[0,4000000000])
+
+    st.plotly_chart(fig)
