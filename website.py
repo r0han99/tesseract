3468dc68874c3f44e6a4ba6c8d8fd851c4635edf
r0han99 <r409067@gmail.com>
2023-04-30 23:29:57 -0600

    added content

--- website.py
+++ website.py
@@ -86,9 +86,8 @@ if category == 'Neural Networks':
         ncol2.markdown("")
         ncol2.markdown(f"Class: <br><b>{y}</b>",unsafe_allow_html=True)
 
-        prediction = model.predict(np.array(X).reshape(-1,7))
-        col1.write(prediction)
-        col1.write(classes)
+        prediction = model.predict(np.array(X).reshape(-1,7))        
+        col1.subheader(f"The Model Categorized this Sample of Data to be: {classes[prediction.argmax()]}")
         
 
 
