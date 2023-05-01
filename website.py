7e7b05f85bb0444c1b1b8b815da04f9dae883622
r0han99 <r409067@gmail.com>
2023-04-30 23:27:37 -0600

    added content

--- website.py
+++ website.py
@@ -38,7 +38,7 @@ def select_sample(data):
 @st.cache_resource
 def read_model():
     
-    model = load_model('CRIME_CLASSIFIER.h5')
+    model = load_model('./CRIME_CLASSIFIER.h5')
     return model
 
 @st.cache_data
